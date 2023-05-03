ce8fc3501b9e4d73413b835ca7654bca1922c34a
Claudio Araujo <calloman123@gmail.com>
2023-05-03 14:34:59 -0600

    Early main page

--- main_page.py
+++ main_page.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.markdown("# Main page")
+st.markdown(
+    """
+## Araujo Palestina Claudio Hassiel
+Demostracion en python sobre decenso de gradientes
+- Batch
+- Mini-Batch
+- Estocastico
+"""
+)
